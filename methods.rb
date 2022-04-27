c3960d36a07b6a251c4a192c651e77f68928c5b8
Steve Alomia <stevealomia@yahoo.com>
2022-04-27 15:25:11 -0400

    commit tree 0df743a85d4e074066a7e837b99cd84c56d8d097

--- methods.rb
+++ methods.rb
@@ -1,1 +1,44 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+greet_with_default "Sunny"
+
+
+def add (num1, num2)
+    num1 + num2 
+end 
+
+sum1 = add(1, 2) 
+#check this later 
+print sum1
+
+def halve number 
+ if number.class != Integer
+    return nil 
+ end
+ number / 2 
+end 
+
+
+halve("six")
+
+halve(6)
+
+
+
